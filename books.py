9d5fc9ae325df03e5a8dd547c50b7dc497c44afc
KitKat420 <danteclark11@hotmail.com>
2023-01-23 05:07:15 -0500

    returns arr of books

--- books.py
+++ books.py
@@ -1,24 +1,43 @@
 import requests
+from typing import Any
 
 
 class Books:
 
     def __init__(self, key):
         self.key = key
+        self.books = []
 
-    def search(self, by, query):
+    def fetch(self, url: str) -> requests.Response:
+        try:
+            res: requests.Response = requests.get(url)
+        except requests.exceptions.HTTPError as e:
+            print(e)
+        else:
+            return res
+
+    def search(self, by: str, query: str):
         """Searches Google Books API
 
         Args:
             by (string): searches by intitle, inauthor, or inpublisher
             query (string): provides descriptive search result for by
         """
+        url: str = f"https://www.googleapis.com/books/v1/volumes?q=search+{by}:{query}&key={self.key}"
+        return self.fetch(url).json()
 
-        url = self.loc_book = f"https://www.googleapis.com/books/v1/volumes?q=search+{by}:{query}&key={self.key}"
+    def book_info(self, book):
 
         try:
-            res = requests.get(url)
-        except requests.exceptions.HTTPError as e:
-            print(e)
+            items = book["items"]
+
+        except KeyError as e:
+            print(f"Key does not exist {e}")
         else:
-            print(res.json())
+            for i in range(len(items) - 1):
+                title: str = book["items"][i]["volumeInfo"]["title"]
+                description: str = book["items"][i]["volumeInfo"]["description"]
+
+                self.books.append({"title": title,
+                                  "description": description})
+            return self.books
