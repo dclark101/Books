197482c9d58df1cb129e2c3d4b3271c1b9e2f79f
KitKat420 <danteclark11@hotmail.com>
2023-01-23 05:06:39 -0500

    no changes

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import books
-
 import os
 from dotenv import load_dotenv
-load_dotenv()
 
+load_dotenv()
 API_KEY = os.environ.get("API_KEY")
 
 google_books = books.Books(API_KEY)
+book_data = google_books.search("intitle", "Harry Potter")
 
-google_books.search("inauthor", "J.K. Rowling")
+print(google_books.book_info(book_data))
