687ba5cdf6f50fe78283563c622767002082bc7f
KitKat420 <danteclark11@hotmail.com>
2023-01-23 06:45:43 -0500

    modified exceptions

--- books.py
+++ books.py
@@ -2,6 +2,9 @@ import requests
 from typing import Any
 
 
+EXCEPTIONS = requests.exceptions
+
+
 class Books:
 
     def __init__(self, key):
@@ -9,10 +12,19 @@ class Books:
         self.books = []
 
     def fetch(self, url: str) -> requests.Response:
+        """Sends a request to a Google Books API endpoint
+
+        Args:
+            url (str): Google Books API endpoint
+
+        Returns:
+            requests.Response: JSON object
+        """
+
         try:
             res: requests.Response = requests.get(url)
-        except requests.exceptions.HTTPError as e:
-            print(e)
+        except EXCEPTIONS.RequestException as e:
+            print("Something went wrong.", e)
         else:
             return res
 
@@ -24,15 +36,21 @@ class Books:
             query (string): provides descriptive search result for by
         """
         url: str = f"https://www.googleapis.com/books/v1/volumes?q=search+{by}:{query}&key={self.key}"
-        return self.fetch(url).json()
+        try:
+            json_data = self.fetch(url).json()
+        except EXCEPTIONS.JSONDecodeError:
+            print("Couldn't decode the text to JSON")
+        else:
+            return json_data
 
     def book_info(self, book):
 
         try:
             items = book["items"]
-
         except KeyError as e:
             print(f"Key does not exist {e}")
+        except TypeError as e:
+            print("book['items']", e)
         else:
             for i in range(len(items) - 1):
                 title: str = book["items"][i]["volumeInfo"]["title"]
